b1cfd4770eb0748e96a016a498bbcd255aa582bb
AmarzitMathias <72758865+AmarzitMathias@users.noreply.github.com>
2025-08-13 02:10:33 +0200

    Vérification path + exception lors création dossier

--- Project_automation.py
+++ Project_automation.py
@@ -2,15 +2,30 @@ import os
 
 print("Veuillez créer le dosssier de votre projet")
 path = input("Veuillez rentrer le chemin où vous souhaitez sauvegarder le projet (chemin absolu) ")
+
+
+# Vérification du chemin
+if not path:
+  print("Le chemin ne peut pas être vide")
+  exit()
+if not os.path.isabs(path):
+  print("Le chemin n'est pas absolu. Veuillez entrer un chemin absolu (ex : /home/user/projets ou C:\\Users\\user\\projets).")
+  exit()
+
+
 name = input("Veuillez rentre le nom de votre projet ")
 print("Le chemin que vous souhaitez est : ", path)
 print("Création du dossier...")
+
 # Construction du chemin complet du dossier projet
 full_path = os.path.join(path, name)
-# check si le dossier existe déjà
+# Vérification si le dossier existe déjà
 if not os.path.exists(full_path):
-  os.mkdir(full_path)
-  # Utilisation de f-string
-  print(f"Dossier {name} a été créé !")
+  try:
+    os.mkdir(full_path)
+    # Utilisation de f-string
+    print(f"Dossier {name} a été créé à l'emplacement : {full_path}")
+  except Exception as error:
+    print(f"Une erreur est survenue lors de la création du dossier : {error}")
 else:
-  print(f"Dossier {name} existe déjà à l'emplacement : {full_path}")
+  print(f"Le dossier {name} existe déjà à l'emplacement : {full_path}")
